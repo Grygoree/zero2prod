0290f45d465c531e50a890e78f31921c53fb4fbf
Grygoree <not.gregs.other.email@gmail.com>
2025-09-10 00:27:05 -0500

    Implement JSONified logs and tracing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 use zero2prod::{configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    //env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    // Use info if RUST_LOG environment variable has not been set.
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     let configuration = configuration::get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
